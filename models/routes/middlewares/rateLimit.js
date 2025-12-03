c596067331145a5c6b40948a9852725e062685d0
brader73 <143875377+brader73@users.noreply.github.com>
2025-12-03 20:05:19 +0300

    Delete models/routes/middlewares/rateLimit.js

--- models/routes/middlewares/rateLimit.js
+++
@@ -1,7 +0,0 @@
-import rateLimit from "express-rate-limit";
-
-export const authLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,  // 15 dakika
-  max: 20,                   // 20 deneme sınırı
-  message: "Çok fazla istek gönderildi, biraz bekleyin.",
-});
